61752ac27c12c8d6ff8b99d6d6d71e7de0749d7a
tiawl <pabtomas@protonmail.com>
2025-05-20 20:37:21 +0200

    more tests

--- test/defer.sh
+++ test/defer.sh
@@ -3,7 +3,7 @@
 source src/utils.sh
 
 defer () {
-  local stage pfx ppfx sep old_ifs prev_return_trap min max
+  local stage pfx ppfx sep old_ifs prev_return_trap min max x
   local -a caller
   old_ifs="${IFS}"
   stage='0'
@@ -22,11 +22,10 @@ defer () {
     caller["${max}"]="${x}"
     (( min++, max-- ))
   done
-  IFS="${sep}"
-  caller="${caller[*]}"
-  IFS="${old_ifs}"
   ppfx="${sep}${sep}deferred${sep}"
-  pfx="${ppfx}${caller}${sep}"
+  IFS="${sep}"
+  pfx="${ppfx}${caller[*]}${sep}"
+  IFS="${old_ifs}"
   readonly pfx ppfx caller
 
   if is not func "${pfx}0"
@@ -44,10 +43,14 @@ fi
   source /proc/self/fd/0 <<< "
     ${pfx}${stage} () {
       local before after
-      before=\"\$(compgen -A function -X '!${ppfx}*0')\"
+      on noglob
+      before=(\$(compgen -A function -X '!${ppfx}*0'))
+      off noglob
       ${*}
-      after=\"\$(compgen -A function -X '!${ppfx}*0')\"
-      if str not eq \"\${before}\" \"\${after}\"
+      on noglob
+      after=(\$(compgen -A function -X '!${ppfx}*0'))
+      off noglob
+      if str not eq \"\${before[*]}\" \"\${after[*]}\"
       then
         local deferred
         for deferred in \$(gojq -n -r '\$ARGS.positional | group_by(.) | .[] | select(length == 1) | .[0]' --args \"\${before[@]}\" \"\${after[@]}\")
@@ -66,7 +69,7 @@ fi
 
 on functrace
 
-@test "simple defer in one function" {
+@test "simple defer" {
   a() {
     defer "echo '${FUNCNAME[0]}: Cleanup'"
     echo "${FUNCNAME[0]}: Init"
@@ -81,7 +84,7 @@ on functrace
   str eq "${lines[2]}" 'a: Cleanup'
 }
 
-@test "multiple defers in one function" {
+@test "multiple defers" {
   a() {
     defer "echo '${FUNCNAME[0]}: Cleanup:'"
     defer "echo '${FUNCNAME[0]}:   Removing temporary file'"
@@ -100,7 +103,7 @@ on functrace
   str eq "${lines[4]}" 'a:   Freeing memory'
 }
 
-@test "multiple defers into nested function 1" {
+@test "nested function 1" {
   b() {
     defer "echo '${FUNCNAME[0]}: Cleanup:'"
     defer "echo '${FUNCNAME[0]}:   Freeing memory'"
@@ -122,7 +125,7 @@ on functrace
   str eq "${lines[3]}" 'a:   Releasing resources'
 }
 
-@test "multiple defers into nested function 2" {
+@test "nested function 2" {
   e() {
     defer "echo '${FUNCNAME[0]}: Cleanup:'"
     defer "echo '${FUNCNAME[0]}:   Cook a cake'"
@@ -170,7 +173,69 @@ on functrace
   str eq "${lines[9]}" 'a:   Releasing resources'
 }
 
-# "recursive" case
+@test "recursive function" {
+  a() {
+    defer "echo '${FUNCNAME[0]} ${1}: Cleanup:'"
+    defer "echo '${FUNCNAME[0]} ${1}:   Removing temporary file'"
+
+    if lt "${1}" '3'
+    then
+      a "$(( ${1} + 1 ))"
+    fi
+  }
+
+  run a 1
+  eq "${status}" '0'
+  eq "${#lines[@]}" '6'
+  str eq "${lines[0]}" 'a 3: Cleanup:'
+  str eq "${lines[1]}" 'a 3:   Removing temporary file'
+  str eq "${lines[2]}" 'a 2: Cleanup:'
+  str eq "${lines[3]}" 'a 2:   Removing temporary file'
+  str eq "${lines[4]}" 'a 1: Cleanup:'
+  str eq "${lines[5]}" 'a 1:   Removing temporary file'
+}
+
+@test "nested defers" {
+  b() {
+    defer "echo '${FUNCNAME[0]}: Cleanup:'"
+    defer "echo '${FUNCNAME[0]}:   Freeing memory'"
+  }
+
+  a() {
+    defer "echo '${FUNCNAME[0]}: Cleanup:'"
+    defer "echo '${FUNCNAME[0]}:   Releasing resources'"
+    defer 'b'
+  }
+
+  run a
+  eq "${status}" '0'
+  eq "${#lines[@]}" '4'
+  str eq "${lines[0]}" 'a: Cleanup:'
+  str eq "${lines[1]}" 'a:   Releasing resources'
+  str eq "${lines[2]}" 'b: Cleanup:'
+  str eq "${lines[3]}" 'b:   Freeing memory'
+}
+
+@test "nested defers into recursive function" {
+  a() {
+    defer "echo '${FUNCNAME[0]} ${1}: Cleanup:'"
+    defer "echo '${FUNCNAME[0]} ${1}:   Removing temporary file'"
+
+    if lt "${1}" '3'
+    then
+      defer "a '$(( ${1} + 1 ))'"
+    fi
+  }
+
+  run a 1
+  eq "${status}" '0'
+  eq "${#lines[@]}" '6'
+  str eq "${lines[0]}" 'a 1: Cleanup:'
+  str eq "${lines[1]}" 'a 1:   Removing temporary file'
+  str eq "${lines[2]}" 'a 2: Cleanup:'
+  str eq "${lines[3]}" 'a 2:   Removing temporary file'
+  str eq "${lines[4]}" 'a 3: Cleanup:'
+  str eq "${lines[5]}" 'a 3:   Removing temporary file'
+}
+
 # "error" case
-# "use in defer in function that use defer" case
-# errdefer ??
